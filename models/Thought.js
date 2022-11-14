9e7f7921abea79d5efce86122a0b558bc6dc37c0
bburton5 <bburton5@uw.edu>
2022-11-13 17:31:18 -0800

    created Thought model

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // TODO: use a getter method to format the timestamp on query
+    },
+    username: {
+      // (The user that created this thought)
+      type: String,
+      required: true,
+    },
+    reactions: {
+      // (These are like replies)
+      // TODO: Array of nested documented created with the reactionSchema
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+module.exports = thoughtSchema;
