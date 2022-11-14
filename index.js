0b52cee9b7a400e64c39ee5932746f0e75714a20
bburton5 <bburton5@uw.edu>
2022-11-13 21:58:36 -0800

    trying to deploy to heroku

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const cwd = process.cwd();
 
 const PORT = process.env.port || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
