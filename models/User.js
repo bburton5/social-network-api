20ad3df10fc9a63dc02ed90431de35e824fc9747
bburton5 <bburton5@uw.edu>
2022-11-13 17:18:56 -0800

    created User model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Schema, Types } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // TODO: must match a valid email address
+    },
+    thoughts: {
+      // TODO: array of _id values referencing the Thought model
+    },
+    friends: {
+      // TODO: array of _id values referencingh the User model (self-reference)
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+module.exports = userSchema;
