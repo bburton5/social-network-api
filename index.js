de639d6ccb446cba1d652f0b8803e76078a24dde
bburton5 <bburton5@uw.edu>
2022-11-13 21:53:21 -0800

    trying to start heroku app

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const cwd = process.cwd();
+
+const PORT = process.env.port || 3001;
+const app = express();
